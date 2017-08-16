77808ffbfa6ed330fa30ac0258365f3830e2ef04
Emre-HomePC <e@mildmania.com>
2017-08-16 18:58:08 +0300

    battle test started!

---
+++ KPCombat/Assets/_Scripts/CharacterInputController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public enum InputEnum
+{
+    Attack,
+
+}
+
+public class CharacterInputController : MonoBehaviour {
+
+    public static Action<InputEnum> OnInput;
+
+    void FireOnInput(InputEnum inputType)
+    {
+        if (OnInput != null)
+            OnInput(inputType);
+    }
+
+    private void Update()
+    {
+        CheckInput();
+    }
+
+    void CheckInput()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+            FireOnInput(InputEnum.Attack);
+
+    }
+}
